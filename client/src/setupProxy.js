4b012f1f694ff77bd1724678e02b4d60ad5e29f1
Henry Goodman <13032204@student.uts.edu.au>
2023-08-17 18:19:04 +1000

    Add logic to set API proxy depending on environment

---
+++ client/src/setupProxy.js
@@ -0,0 +1,20 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const { dotenv } = require("dotenv").config({path:'../.env'})
+
+module.exports = function(app) {
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+  console.log('[client]: Environment is:' , process.env.REACT_ENV ? process.env.REACT_ENV : 'development')
+  const target =
+    process.env.REACT_ENV == 'production'
+      ? 'https://www.google.com'      // Your production backend URL (currently not used)
+      : `http://localhost:${port}`;   // Your default development backend URL
+
+  console.log('[client]: Backend API @' , target)
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target,
+      changeOrigin: true,
+    })
+  );
+};
