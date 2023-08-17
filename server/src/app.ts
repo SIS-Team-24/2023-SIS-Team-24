6d545d36bc7359c1278d6ee54a09f5ab75423102
Henry Goodman <13032204@student.uts.edu.au>
2023-08-17 18:19:04 +1000

    Add logic to set API proxy depending on environment

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response } from "express";
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({path:'../.env'});
 
 const app = express();
-const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8000;
 
-app.get('/', (req: Request, res: Response) => {
+const port: number = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+
+app.get('/api', (req: Request, res: Response) => {
   res.send('⚡️⚡️⚡️ Express + TypeScript Server! ⚡️⚡️⚡️');
 });
 
