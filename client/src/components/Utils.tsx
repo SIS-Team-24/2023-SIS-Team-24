1747262fe6f5035281777731ca074b78bd043c42
rgonzales4 <roshel.gonzales@logilica.com>
2023-08-31 23:58:00 +1000

    feat: :sparkles: Created basic function to store and retrieve analysis output from sessionStorage

--- client/src/components/Utils.tsx
+++ client/src/components/Utils.tsx
@@ -1,8 +1,40 @@
-/* 
-    WIP: May delete this file but for now...
-    Insert all reusable variables here
-*/
+// Essential props for POST request
 export const postRequestOptions = {
   method: "POST",
-  headers: { "Content-Type": "application/json"},
+  headers: { "Content-Type": "application/json" },
 };
+
+export interface AnalysisInput {
+  summary: string;
+  // Sentiment can potential be undefined, depending on the type of text that is inputted
+  // Referencing previous discussion on how some texts may not have sentiment (e.g. stories)
+  sentiment?: string;
+}
+
+export interface AnalysisOutput extends AnalysisInput {
+  timestamp: number;
+}
+
+// Note 01: Referencing previous testing on the summary analysis, the summary of an input
+// will always be the same, and due to the current agreement on "it's okay if the summary is the same".
+// Thus, an ID won't be necessary there'll only be one summary per input
+// (i.e there won't be various summaries to relate to a single input, one-to-one relo)
+export function addToHistory(analysis: AnalysisInput) {
+  const historyArr: AnalysisOutput[] = JSON.parse(sessionStorage.getItem("history") || "[]");
+  // If summary does not exist in history, store into history...
+  // prevent double storage of the same history, refer to Note 01
+  if (!historyArr.find((h) => h.summary === analysis.summary)) {
+    historyArr.push({ timestamp: Date.now(), ...analysis });
+  }
+  sessionStorage.setItem("history", JSON.stringify(historyArr));
+}
+
+// Retrieve data from sessionStorage (string), then convert into appropriate data type (JSON.parse)
+export function retrieveHistory(): AnalysisOutput[] {
+  return JSON.parse(sessionStorage.getItem("history") || "[]");
+}
+
+// Set history to empty arr
+export function clearHistory() {
+  sessionStorage.setItem("history", "[]");
+}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Button from "../components/Button";
-import { postRequestOptions } from "../components/Utils";
+import { addToHistory, clearHistory, postRequestOptions, retrieveHistory } from "../components/Utils";
 
-function Home() {
+function Home(this: any) {
   const [someState, setSomeState] = useState("not set");
   const [textInput, setTextInput] = useState("Placeholder");
+  const [testTextInput, setTestTextInput] = useState("Insert text here to test analysis storage function");
 
   useEffect(() => {
     console.log(`useEffect placeholder... initial state: ${someState}`);
@@ -15,30 +16,50 @@ function Home() {
 
   // Testing function to call Summary analysis fn...
   const getSummary = () => {
-    const body = JSON.stringify({"text": "testing summary/process ..."})
-    fetch("/api/summary/process",{...postRequestOptions, body}).then(response => response.json())
-    .then(data => {
-      console.log(data)
-      if(data.summary){
-        setTextInput("Summary API call was successful.");
-      } else {
-        setTextInput("Call to /api/summary/process failed.");
-      }
-    });
+    const body = JSON.stringify({ text: "testing summary/process ..." });
+    fetch("/api/summary/process", { ...postRequestOptions, body })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.summary) {
+          setTextInput("Summary API call was successful.");
+        } else {
+          setTextInput("Call to /api/summary/process failed.");
+        }
+      });
   };
 
   // Testing function to call Sentiment analysis fn...
   const getSentiment = () => {
-    const body = JSON.stringify({"text": "testing sentiment/process ..."})
-    fetch("/api/sentiment/process",{...postRequestOptions, body}).then(response => response.json())
-    .then(data => {
-      console.log(data)
-      if(data.sentiment){
-        setTextInput("Sentiment API call was successful.");
-      } else {
-        setTextInput("Call to /api/sentiment/process failed.");
-      }
-    });
+    const body = JSON.stringify({ text: "testing sentiment/process ..." });
+    fetch("/api/sentiment/process", { ...postRequestOptions, body })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.sentiment) {
+          setTextInput("Sentiment API call was successful.");
+        } else {
+          setTextInput("Call to /api/sentiment/process failed.");
+        }
+      });
+  };
+
+  // Test function for capturing text for testing storage function...
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTestTextInput(event.target.value);
+  };
+
+  // Example of how to use addToHistory() - Summary only...
+  const testStoringToHistory = () => {
+    // param type = AnalysisInput (refer to Utils.tsx)
+    addToHistory({ summary: testTextInput });
+  };
+
+  // Example of how to use retrieveHistory()...
+  const getAnalysisHistory = () => {
+    const historyArr = retrieveHistory();
+    // Do some formatting...
+    setTextInput(JSON.stringify(historyArr));
   };
 
   return (
@@ -55,6 +76,25 @@ function Home() {
         </Button>
         <Button variant="active" onClick={getSentiment}>
           Sentiment Test
+        </Button>
+      </div>
+      <div className="flex justify-center gap-5 p-10">
+        <textarea value={testTextInput} onChange={(value) => handleChange(value)} className="w-100" />
+        <Button variant="active" onClick={testStoringToHistory}>
+          Store text into history
+        </Button>
+        <Button variant="active" onClick={getAnalysisHistory}>
+          Retrieve History
+        </Button>
+        <Button
+          variant="active"
+          onClick={() => {
+            // Example of to clear history and show some kind of message to user...
+            clearHistory();
+            setTextInput("History Cleared.");
+          }}
+        >
+          Clear History
         </Button>
       </div>
       {textInput && <p className="flex justify-center">{textInput}</p>}
