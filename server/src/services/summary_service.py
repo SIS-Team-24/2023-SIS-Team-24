7502db02c0533634a40c2fbb4e5e1094a906013b
Henry <13032204@student.uts.edu.au>
2023-08-31 18:50:54 +1000

    Load model as initialization step

--- server/src/services/summary_service.py
+++ server/src/services/summary_service.py
@@ -1,14 +1,20 @@
 import sys
+import os
 from happytransformer import HappyTextToText, TTSettings
+
+
+# Load the model upon server initialization
+global happy_tt1
+happy_tt1 = HappyTextToText("BERT", "sshleifer/distilbart-cnn-12-6")
+print(f"[server] Loaded Model in {os.path.basename(__file__)}")
 
 def get_summary(input_text:str):
     """
     Generate a summary for the input text.
     """
     print("[server] Function to generate summary is executing.")
-
-    happy_tt1 = HappyTextToText("BERT", "sshleifer/distilbart-cnn-12-6")
-    result1 = happy_tt1.generate_text(input_text, args=TTSettings(min_length=5, max_length=500))
+    global happy_tt1
+    result1 = happy_tt1.generate_text(input_text, args=TTSettings(min_length=1, max_length=500))
     return result1.text
 
 if __name__ == '__main__':
