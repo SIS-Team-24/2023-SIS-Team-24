f3736c12426c1f7c20b66d16804a2c6d6f5761e1
SSBdevelopment <stephenbrown.business@gmail.com>
2023-09-04 13:47:27 +1000

    feat: use sentiment analysis model

--- server/src/services/sentiment_service.py
+++ server/src/services/sentiment_service.py
@@ -1,8 +1,14 @@
+from transformers import pipeline
+
 def get_sentiment(input_text:str):
     """
     Generate a sentiment for the input text.
     """
-    # For this simple example, let's just return the input text as the summary.
     print("[server] Function to generate sentiment is executing.")
 
-    return input_text
+    # Model trained with SemEval 2017 corpus (around ~40k tweets)
+    # Return "POS", "NEU" or "NEG" and the sentiment score
+    sentiment_analyzer=pipeline('sentiment-analysis', model="finiteautomata/bertweet-base-sentiment-analysis")
+    result=sentiment_analyzer(input_text)
+
+    return result
