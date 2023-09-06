98a7863b7f1ee1fb8bdb77e06a92eb3f8e13e137
Roshel Gonzales <roshel.gonzales@yahoo.com>
2023-09-06 15:57:31 +1000

    refactor: :recycle: Refined get_summary(),  Added comments, changed summarisation model, err handling (#51)

--- server/src/services/summary_service.py
+++ server/src/services/summary_service.py
@@ -1,30 +1,34 @@
-import sys
 import os
 from happytransformer import HappyTextToText, TTSettings
 
-
 # Load the model upon server initialization
 global happy_tt1
-happy_tt1 = HappyTextToText("BERT", "sshleifer/distilbart-cnn-12-6")
-print(f"[server] Loaded Model in {os.path.basename(__file__)}")
+model_type = "BERT"
+model_name = "facebook/bart-large-cnn" # Source: https://huggingface.co/facebook/bart-large-cnn
+happy_tt1 = HappyTextToText(model_type, model_name)
+print(f"[server] Loaded Model {model_name}, type {model_type}, in {os.path.basename(__file__)}")
 
 def get_summary(input_text:str):
     """
     Generate a summary for the input text.
     """
     print("[server] Function to generate summary is executing.")
-    global happy_tt1
-    result1 = happy_tt1.generate_text(input_text, args=TTSettings(min_length=1, max_length=500))
-    result1 = postprocess_replace_space_dot(result1.text)
-    return result1
 
-def postprocess_replace_space_dot(input_string):
-    # Replace ' .' with '.'
-    result_string = input_string.replace(' .', '.')
-    return result_string
+    # Summarisation settings
+    summary_args = TTSettings(min_length=100, max_length=500)
 
+    # Summary generator
+    result = happy_tt1.generate_text(input_text, summary_args)
 
+    # Handle err if results are not found
+    try:
+        getattr(result, "text")
+        return result.text
+    except AttributeError:
+        return "ERROR: failed to summarise text."
+    
+# TODO: delete after function finalisation...
 if __name__ == '__main__':
-    # Ensure input.txt exists in root for testing.
-    with open('input.txt') as f:
+    # Ensure input.txt exists in server/src for testing.
+    with open('input.txt', 'r', errors='ignore') as f:
         print(get_summary(str(f.read())))
