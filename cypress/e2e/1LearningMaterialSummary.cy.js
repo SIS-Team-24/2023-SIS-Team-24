db31e05d016999a82a1a0cc6558cbafaaf818f2c
gracebilliris <grace.billiris@gmail.com>
2023-09-08 21:38:00 +1000

    Updating Cypress test to assert

--- cypress/e2e/1LearningMaterialSummary.cy.js
+++ cypress/e2e/1LearningMaterialSummary.cy.js
@@ -2,17 +2,16 @@ describe('Learning Material Summary', () => {
     it('Going to Learning Material Summary Page', () => {
         cy.visit('/')
         cy.contains('Learning Material Summary').click();
-        cy.wait(2000)
+        
+        cy.url().should('include', 'learningMaterialSummary')
 
         cy.contains('What dataset was Text Insights trained on?').click();
-        cy.contains('BERT')
-        cy.wait(2000)
-
+        cy.get('#0').should('contain', 'BERT');
+        
         cy.contains('What dataset was Text Insights trained on?').click();
         cy.contains('How does BERT work?').click();
-        cy.contains('BERT')
+        cy.get('#1').should('contain', 'BERT');
         
-        cy.wait(2000)
         cy.contains('How does BERT work?').click();
     })
 })
