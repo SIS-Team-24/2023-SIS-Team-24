9a37ff7b6e3e380534500fbbc50bd544ab63069a
Roshel Gonzales <roshel.gonzales@yahoo.com>
2023-09-09 17:55:36 +1000

    [SIS-128] Refine the sentiment analysis (#54)
    * feat: use sentiment analysis model
    
    * feat: :poop: Added tests for the different / potential senitment analysis models
    
    * feat: :sparkles: Refined sentiment analysis
    
    Changed the sentiment analysis model to allow a larger word limit + introduced logic to handle input if word limit is exceeded
    
    * refactor: :recycle: propogate server err to frontend
    
    * refactor: :recycle: improve loading for sentiment analysis
    
    * feat: :sparkles: added sentiment model initialisation to main.py
    
    ---------
    
    Co-authored-by: SSBdevelopment <stephenbrown.business@gmail.com>

--- server/src/services/sentiment_service.py
+++ server/src/services/sentiment_service.py
@@ -1,11 +1,45 @@
+import os
+from transformers import pipeline
+
+# Map to return worded sentiment.
+sentiment_map = {
+    "LABEL_0": "Negative",
+    "LABEL_1": "Neutral",
+    "LABEL_2": "Positive"
+}
+
+# Load the machine learning model during app startup
+def load_model():
+    global sentiment_analyser
+    model_name = "cardiffnlp/twitter-roberta-base-sentiment" # Source: https://huggingface.co/cardiffnlp/twitter-roberta-base-sentiment
+    sentiment_analyser=pipeline('sentiment-analysis', model_name)
+    print(f"[server] Loaded Model {model_name} in {os.path.basename(__file__)}")
+
 def get_sentiment(input_text:str):
     """
     Generate a sentiment for the input text.
     """
-    # For this simple example, let's just return the input text as the summary.
-    print("[server] Function to generate sentiment is executing.")
+    print("[server] Function to generate sentiment is executing.")  
 
-    return input_text
+    # Default: Set text_to_analyse to current input_text
+    text_to_analyse = input_text
+
+    # If number of words in input_text is >400, get summary of input_text
+    input_len = len(input_text.split())
+    if input_len > 400:
+        # Load get_summary() only when this condition is met.
+        from summary_service import get_summary
+        # TODO: currently get_summary() is set to return a summary of 100 words - change following implementation upon related project progress.
+        print(f"Inputted text is too long, {input_len} words. Summarising content to run sentiment analysis.")
+        text_to_analyse = get_summary(input_text)
+
+    # Execute sentiment analyzer, and handle any err.   
+    result=sentiment_analyser(text_to_analyse)
+    
+    # analyser returns an arr of results - take first index and return value.
+    return {"sentiment": sentiment_map[result[0]["label"]], 'score': result[0]["score"]}
+
+
 
 def get_emotion(input_text:str):
     """
@@ -14,6 +48,11 @@ def get_emotion(input_text:str):
     # For this simple example, let's just return the input text as the summary.
     print("[server] Function to generate emotion analysis is executing.")
 
-    
-
     return input_text
+
+# TODO: delete after function finalisation...
+if __name__ == '__main__':
+    # Ensure input.txt exists in server/src for testing.
+    load_model()
+    with open('input.txt', 'r', errors='ignore') as f:
+        print(get_sentiment(str(f.read())))     
