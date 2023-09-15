efa653ff9d3d11b44eeed09b3d3af916c698f391
Henry <13032204@student.uts.edu.au>
2023-09-15 11:59:17 +1000

    Add static URL

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
   console.log('[client]: Environment is:' , process.env.REACT_ENV ? process.env.REACT_ENV : 'development')
   const target =
     process.env.REACT_ENV == 'production'
-      ? 'https://a762-125-168-205-95.ngrok-free.app'      // Your production backend URL (currently not used)
+      ? 'https://goat-thankful-iguana.ngrok-free.app'      // Your production backend URL (currently not used)
       : `http://127.0.0.1:${port}`;   // Your default development backend URL
 
   console.log('[client]: Backend API @' , target)
