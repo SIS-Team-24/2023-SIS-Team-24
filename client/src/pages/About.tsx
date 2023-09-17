3cb919ada0541895a9f812efd1e0c8b44b224fe0
Kyrus2504 <anony100263@gmail.com>
2023-09-17 19:34:35 +1000

    added content to about page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -6,7 +6,19 @@ function About(this: any) {
     <div className="mt-10">
       <NavigationBar />
 
-      <p>Test</p>
+      <p className="flex items-center items-baseline justify-start space-x-4 text-xs ml-60">
+        Text Insights is your go-to solution for harnessing the power of Natural
+        Language Processing (NLP). Our product empowers users to effortlessly
+        summarise and analyse text content, providing invaluable insights.
+        Whether you're a student grappling with voluminous textbooks, an
+        educator seeking efficient content evaluation, or a professional
+        navigating the digital landscape, Text Insights has you covered. We
+        prioritise simplicity, customisation, and accuracy, making complex tasks
+        a breeze. Powered by open-source models, our tool offers an ethical and
+        transparent approach to text processing. With Text Insights, you unlock
+        a world of concise understanding and sentiment analysis, enriching your
+        text-related endeavours.
+      </p>
     </div>
   );
 }
