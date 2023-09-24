d18cd2d1c6b1739421aea0af254b2edbeacab0a9
srujan <srujan.sharma77k@gmail.com>
2023-09-25 00:39:01 +1000

    added in min validation for text input to a 100 words

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -16,6 +16,7 @@ function Home(this: any) {
   const [selectedFont, setSelectedFont] = useState<string | null>(null);
   const [emotionLabel, setEmotionLabel] = useState("Not set"); // "Happy", "Sad", "angry"
   const [submitted, setSubmitted] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true); // Add button state
 
   const setSentimentStyle = () => {
     switch (sentimentText) {
@@ -76,6 +77,18 @@ function Home(this: any) {
           borderRadius: "4px",
         };
     }
+  };
+
+  useEffect(() => {
+    // Calculate word count when inputValue changes
+    const wordCount = calcWordCount(inputValue);
+    setIsButtonDisabled(wordCount < 100);
+  }, [inputValue]); // Run this effect whenever inputValue changes
+
+  // Calc word count
+  const calcWordCount = (text: any) => {
+    const words = text.trim().split(/\s+/);
+    return words.length;
   };
 
   // Event handler for input value change
@@ -203,9 +216,14 @@ function Home(this: any) {
             </div>
           </div>
           <button
+            id="summarise-button-id"
             onClick={getSummary}
-            style={{ backgroundColor: "#2e7faa" }}
+            style={{
+              backgroundColor: "#2e7faa",
+              cursor: isButtonDisabled ? "not-allowed" : "pointer",
+            }}
             className="mt-8 ml-52 py-2 px-4 text-white rounded"
+            disabled={isButtonDisabled}
           >
             Summarise
           </button>
