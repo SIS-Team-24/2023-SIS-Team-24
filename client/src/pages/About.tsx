b48317d83a0c3bf42bb0bdb2a6fde79495d38de1
JamesBug404 <79614873+JamesBug404@users.noreply.github.com>
2023-10-09 13:15:30 +1100

    Update About.tsx

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -46,11 +46,7 @@ function About(this: any) {
 
         
         <div className="mt-10 text-center" >
-          <button style={{
-                backgroundColor: "#2e7faa",
-               
-              }} 
-              className="mt-8 py-2 px-4 text-white rounded">
+          <button className="mt-8 py-2 px-4 text-white rounded bg-cyan-700">
             <a href="/">Try Text Insights</a>
           </button>
         </div>
