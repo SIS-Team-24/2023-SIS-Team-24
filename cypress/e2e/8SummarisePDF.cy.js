fd948393677387dada54ed4e8d0bbf8f1ef77e06
srujan <srujan.sharma77k@gmail.com>
2023-10-21 11:26:11 +1100

    create cypress test for summarise pdf

---
+++ cypress/e2e/8SummarisePDF.cy.js
@@ -0,0 +1,23 @@
+describe("Cypress Testing PDF", () => {
+  it("Summarise PDF", () => {
+    // visit our beautiful website
+    cy.visit("/");
+
+    // check if the summarise button is disabled
+    cy.get("#summarise-button-id").should("be.disabled");
+
+    // wait
+    cy.wait(1000);
+
+    // call my readPdf custom function (cypress.config.ts) on task
+    cy.task(
+      "readPdf",
+      Cypress.config("fileServerFolder") + "/cypress/case-study.pdf"
+    ).then((pdfText) => {
+      // Input/Type it on to the text area
+      cy.get("#inputted-text").type(pdfText);
+
+      cy.get("#summarise-button-id").click();
+    });
+  });
+});
